974d542586adc5033fc8da931092009c79dcb8c5
Lavender <susie204@naver.com>
2024-08-10 11:49:50 +0900

    gitignore Setting

---
+++ LevelMax/LevelMax/LevelMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelMaxApp.swift
+//  LevelMax
+//
+//  Created by 이승현 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LevelMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
